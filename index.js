ccc0b34376b3cb94166beeb8ff0dfc77b31352b8
prajwalMDa <prajwaldevali9@gmail.com>
2025-04-06 21:15:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-if (typeof process !== 'undefined' && !process.browser && process.platform !== 'browser' && parseInt(process.versions.node.split('.')[0]) < 18) {
-  console.error('Your node version is currently', process.versions.node)
-  console.error('Please update it to a version >= 22.x.x from https://nodejs.org/')
-  process.exit(1)
-}
 
-module.exports = require('./lib/loader.js')
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+  host: 'Nether_Forge.aternos.me',
+  port: 22452,
+  username: 'PrajwalAFK', // You can change this name if you want
+  version: false, // Auto-detects server version
+});
+
+bot.once('spawn', () => {
+  console.log('Bot has joined the server!');
+  bot.chat('I am AFK!');
+});
+
+// Prevent bot from getting kicked for idling
+setInterval(() => {
+  bot.setControlState('jump', true);
+  setTimeout(() => bot.setControlState('jump', false), 500);
+}, 10000);
+
+// Reconnect if disconnected
+bot.on('end', () => {
+  console.log('Bot disconnected. Reconnecting...');
+  setTimeout(() => {
+    require('child_process').fork(__filename);
+  }, 5000);
+});
