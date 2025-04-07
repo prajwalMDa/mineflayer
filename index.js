0e2ae468e493cd2c3411376caca574ec12eb429b
prajwalMDa <prajwaldevali9@gmail.com>
2025-04-07 07:13:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
 const mineflayer = require('mineflayer');
-const { Vec3 } = require('vec3');
 
 const bot = mineflayer.createBot({
   host: 'Nether_Forge.aternos.me',
   port: 22452,
-  username: 'PrajwalAFK', // Change name if needed
-  version: false, // Auto-detect version
+  username: 'PrajwalAFK', // You can change this name if you want
+  version: false, // Auto-detects server version
 });
 
-// When bot joins the server
 bot.once('spawn', () => {
   console.log('Bot has joined the server!');
   bot.chat('I am AFK!');
 });
 
-// Auto AFK jumping to avoid kick
+// Prevent bot from getting kicked for idling
 setInterval(() => {
   bot.setControlState('jump', true);
   setTimeout(() => bot.setControlState('jump', false), 500);
 }, 10000);
-
-// Auto sleep at night
-setInterval(() => {
-  if (!bot.time.isDay) {
-    const bed = bot.findBlock({
-      matching: block => bot.isABed(block),
-      maxDistance: 32
-    });
-
-    if (bed) {
-      bot.chat('Trying to sleep...');
-      bot.sleep(bed).then(() => {
-        bot.chat('Good night!');
-      }).catch(err => {
-        bot.chat("Couldn't sleep: " + err.message);
-      });
-    } else {
-      bot.chat('No bed nearby!');
-    }
-  }
-}, 15000);
 
 // Reconnect if disconnected
 bot.on('end', () => {
@@ -48,3 +25,4 @@ bot.on('end', () => {
     require('child_process').fork(__filename);
   }, 5000);
 });
+
