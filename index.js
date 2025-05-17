ae3a0450a0e211a79d63f1003ee2ce3881fb1db8
prajwalMDa <prajwaldevali9@gmail.com>
2025-05-17 08:25:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,54 +27,13 @@ function startBot(index) {
     }, 3000); // Change direction every 3 seconds
   }
 
-  function stopWalking() {
-    walking = false;
-    clearInterval(walkTimer);
-    walkTimer = null;
-    ['forward', 'back', 'left', 'right'].forEach(d => bot.setControlState(d, false));
-  }
-
-  function handleSleeping() {
-    setInterval(() => {
-      const time = bot.time.timeOfDay;
-
-      if (time >= 12000 && time <= 23000) { // Nighttime (12:00 - 23:00)
-        stopWalking();
-        const bed = bot.findBlock({
-          matching: block => bot.isABed(block),
-          maxDistance: 20 // Increased from 10 to 20
-        });
-
-        if (bed) {
-          bot.sleep(bed).then(() => {
-            bot.chat('Sleeping...');
-          }).catch(err => {
-            bot.chat("Couldn't sleep: " + err.message);
-          });
-        } else {
-          bot.chat("No bed found within 20 blocks.");
-        }
-      } else {
-        if (bot.isSleeping) {
-          bot.wake().then(() => {
-            bot.chat('Woke up, back to walking!');
-            startConstantWalking();
-          });
-        } else {
-          startConstantWalking();
-        }
-      }
-    }, 30000); // Check every 30 seconds
-  }
-
   bot.once('spawn', () => {
     console.log(`Bot ${bot.username} has joined.`);
-    
-    bot.chat('/login 1984');  // <-- Added this line for auto-login, replace yourpassword
-    
+
+    bot.chat('/login 1984');  // Replace with your password
+
     bot.chat(`Hello! I am ${bot.username}`);
     startConstantWalking();
-    handleSleeping();
 
     if (currentBot && currentBot !== bot) {
       console.log(`Disconnecting old bot: ${currentBot.username}`);
@@ -83,18 +42,17 @@ function startBot(index) {
 
     currentBot = bot;
 
-    // Set the next bot to join after 30 minutes
+    // Rotate bots every 30 minutes
     setTimeout(() => {
       const nextIndex = (index + 1) % usernames.length;
-      startBot(nextIndex); // Start the next bot
-    }, 30 * 60 * 1000); // 30 minutes
+      startBot(nextIndex);
+    }, 30 * 60 * 1000);
   });
 
   bot.on('end', () => {
     console.log(`${bot.username} disconnected.`);
     if (currentBot === bot) currentBot = null;
 
-    // When the current bot disconnects, immediately start the next bot
     const nextIndex = (index + 1) % usernames.length;
     startBot(nextIndex);
   });
@@ -109,10 +67,18 @@ function startBot(index) {
 
   bot.on('chat', (username, message) => {
     if (username === bot.username) return;
+
     if (message === '!stop') {
       bot.chat('Stopping now...');
       bot.quit();
       process.exit();
+    }
+
+    if (message === '!change') {
+      bot.chat('Changing bot now...');
+      bot.quit();
+      const nextIndex = (index + 1) % usernames.length;
+      startBot(nextIndex);
     }
   });
 }
